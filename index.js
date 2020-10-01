706eaa83044d2af0e89b7310ab26556189e7b993
ralph <ralph.kok@random.nu>
2020-10-01 17:11:32 +0200

    Opening external links in a controlled new browser window. Loading icons from path based on environment (debug or packaged).

--- index.js
+++ index.js
@@ -1,28 +1,103 @@
 const path = require('path');
 const { menubar } = require('menubar');
+const { BrowserWindow } = require('electron');
 
-const getTodaysIcon = (appPath) => path.resolve(
-  appPath,
-  '..',
-  `icons/tray/icon_${(new Date().getDate()).toString().padStart(2, '0')}.png`
-);
+const commandLineArgs = require('command-line-args');
+const args = commandLineArgs([
+  { name: 'debug', alias: 'd', type: Boolean }
+]);
 
+/**
+ * Get icon path based on day of the month.
+ * @param {string} appPath 
+ */
+const getTodaysIcon = (appPath) => args.debug
+  ? path.resolve(
+      `icons/tray/icon_${(new Date().getDate()).toString().padStart(2, '0')}.png`
+    )
+  : path.resolve(
+      appPath,
+      '..',
+      `icons/tray/icon_${(new Date().getDate()).toString().padStart(2, '0')}.png`
+    );
+
+/**
+ * Update the tray icon.
+ * @param {Tray} tray 
+ * @param {string} appPath 
+ */
 const setIcon = (tray, appPath) => {
   tray.setImage(getTodaysIcon(appPath));
 }
 
+/**
+ * Open new windows in a separate, movable, closable window.
+ * @param {*} event 
+ * @param {*} url 
+ * @param {*} frameName 
+ * @param {*} disposition 
+ * @param {*} options 
+ * @param {*} additionalFeatures 
+ * @param {*} referrer 
+ * @param {*} postBody 
+ */
+const onNewWindow = (event, url, frameName, disposition, options, additionalFeatures, referrer, postBody) => {
+  event.preventDefault();
+  const win = new BrowserWindow({
+    webContents: options.webContents, // use existing webContents if provided
+    show: false,
+    autoHideMenuBar: false,
+    movable: true,
+    resizable: true,
+    focusable: true,
+    closable: true,
+  });
+  win.once('ready-to-show', () => win.show());
+  if (!options.webContents) {
+    const loadOptions = {
+      httpReferrer: referrer
+    };
+    if (postBody != null) {
+      const { data, contentType, boundary } = postBody;
+      loadOptions.postData = postBody.data;
+      loadOptions.extraHeaders = `content-type: ${contentType}; boundary=${boundary}`;
+    }
+
+    win.loadURL(url, loadOptions); // existing webContents will be navigated automatically
+  }
+  event.newGuest = win;
+};
+
+/**
+ * Create the main menubar application.
+ */
 const mb = menubar({
   dir: path.resolve(__dirname),
   index: `https://google.com/calendar`,
   browserWindow: {
     width: 600,
-    height: 800
+    height: 800,
+    movable: true,
+    resizable: true,
+    focusable: true
   }
 });
 
+/**
+ * Update the tray when the app is ready.
+ */
 mb.on('ready', () => {
+  // Set the tray icon using today's date
   setIcon(mb.tray, mb.app.getAppPath());
+  // Check every 30s to see if tray icon needs updating to today's date
   setInterval(() => {
     setIcon(mb.tray, mb.app.getAppPath());
-  }, 10000);
+  }, 30000);
+});
+
+/**
+ * Listen for new windows being opened.
+ */
+mb.on('after-create-window', () => {
+  mb.window.webContents.on('new-window', onNewWindow);
 });
